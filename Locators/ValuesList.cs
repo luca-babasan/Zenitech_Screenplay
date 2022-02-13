a6c552857daccb3fae6bc3aa7b5934f90187c45f
luca-babasan <87650604+luca-babasan@users.noreply.github.com>
2022-02-13 14:38:10 +0200

    Update ValuesList.cs

--- Locators/ValuesList.cs
+++ Locators/ValuesList.cs
@@ -4,7 +4,7 @@
     {
         //We can have a list of predefined values to use so our testcases are more readable and make more sense
         public static string Country = "Romania";
-        public static string Phone = "0748533618";
+        public static string Phone = "0741234567";
         public static string First_name = "Luca";
         public static string Last_name = "Babasan";
         public static string Email_field = "luca.babasan@zenitech.co.uk";
